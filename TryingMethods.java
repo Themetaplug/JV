b6dfd232b39fa6cf307732d3a6847f14e62087e7
Oraibi Imabibo <oraibi@meltwater.org>
2022-10-20 08:49:33 +0000

    add main file

---
+++ TryingMethods.java
@@ -0,0 +1,17 @@
+public class TryingMethods {
+    // class for doing the assignment
+    public static void main(String[] args) {
+        
+        
+        summer();}
+
+    public static void summer() {
+        int myCount = 1;
+        int sum = 0;
+        while (myCount < 1000) {
+            myCount += 1;
+            sum += myCount;
+        }
+        System.out.println(sum);
+    }
+}
